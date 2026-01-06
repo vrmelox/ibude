84737061cc42da6e51d72b44d26f3e1a8e305baa
vrmelo <akandeabiodoun@gmail.com>
2026-01-06 21:56:23 +0100

    Add web folder as normal folder

---
+++ web/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <main className="">
+        <h1 className="">Ibude</h1>
+      </main>
+    </div>
+  );
+}

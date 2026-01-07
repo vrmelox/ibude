510dadf1cfdb61be9dc9f648ac0d896d1c663abf
vrmelo <akandeabiodoun@gmail.com>
2026-01-07 19:02:06 +0100

    refactor : correct a conflit SQL command about NOT NULL and ON DELETE SET NULL

--- database/migrations/000005_create_booking_history.up.sql
+++ database/migrations/000005_create_booking_history.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE booking_history(
     id SERIAL PRIMARY KEY,
-    booking_id INT NOT NULL REFERENCES bookings(id) ON DELETE SET NULL,
+    booking_id INT REFERENCES bookings(id) ON DELETE SET NULL,
     old_status booking_status NOT NULL,
     new_status booking_status NOT NULL,
     changed_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    changed_by INT NOT NULL REFERENCES users(id) ON DELETE SET NULL,
+    changed_by INT REFERENCES users(id) ON DELETE SET NULL
 );
 
 -- Create index pour rechercher l'historique d'un booking

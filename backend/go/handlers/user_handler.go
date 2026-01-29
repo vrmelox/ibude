8fb6939a592d41c2bed919efb38a4248459e48b7
vrmelo <akandeabiodoun@gmail.com>
2026-01-29 12:16:32 +0100

    feat: add DeleteUser handler to allow deleting users by ID.

--- backend/go/handlers/user_handler.go
+++ backend/go/handlers/user_handler.go
@@ -133,3 +133,28 @@ func CreateUser(c *gin.Context) {
 		"user": user,
 	})
 }
+
+func DeleteUser(c *gin.Context) {
+	id := c.Param("id")
+	var delUser models.User
+
+	result := config.DB.Delete(&delUser, "id = ?", id)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error": "User not found",
+			})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Database error",
+			"details": result.Error.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "User successfully deleted",
+		"user": delUser,
+	})
+}

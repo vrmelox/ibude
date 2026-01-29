d9ef39ce61e1c59de1c2f83ffe7543546ebc297b
vrmelo <akandeabiodoun@gmail.com>
2026-01-29 19:24:59 +0100

    feat: introduce axios for API requests and centralize API client configuration.

--- web/src/api/auth.ts
+++ web/src/api/auth.ts
@@ -1,7 +1,7 @@
 import { CreateUser, ResponseCreateUser } from "@/types/types";
-
-
+import api from "./api";
 
 export const SendCreateUser = async (data: CreateUser): Promise<ResponseCreateUser> => {
-    const response = await 
+    const response = await api.post<ResponseCreateUser>('/users', data);
+    return response.data;
 }

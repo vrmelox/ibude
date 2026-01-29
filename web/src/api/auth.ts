92ec504c39553a5c24e1c0f746b025de332f7860
vrmelo <akandeabiodoun@gmail.com>
2026-01-29 19:16:40 +0100

    feat: Add initial user creation API function `SendCreateUser` to `auth.ts`.

---
+++ web/src/api/auth.ts
@@ -0,0 +1,7 @@
+import { CreateUser, ResponseCreateUser } from "@/types/types";
+
+
+
+export const SendCreateUser = async (data: CreateUser): Promise<ResponseCreateUser> => {
+    const response = await 
+}

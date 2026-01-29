80b19aa0d046af34aa0a1ca967030451d8647f8c
vrmelo <akandeabiodoun@gmail.com>
2026-01-29 03:13:10 +0100

    feat: add endpoint to find a user by ID with error handling for not found records and database issues.

--- backend/go/handlers/user_handler.go
+++ backend/go/handlers/user_handler.go
@@ -1,11 +1,13 @@
 package handlers
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/vrmelo/ibude/config"
 	"github.com/vrmelo/ibude/models"
+	"gorm.io/gorm"
 )
 
 func GetUsers(c *gin.Context) {
@@ -27,6 +29,32 @@ func GetUsers(c *gin.Context) {
 		"users": users,
 		"count": len(users),
 	})
+}
+
+func FindUserById(c *gin.Context) {
+	id := c.Param("id")
+
+	var user models.User
+	result := config.DB.Where("id = ?", id).First(&user)
+
+    if result.Error != nil {
+        if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+            c.JSON(http.StatusNotFound, gin.H{
+                "error": "User not found",
+            })
+            return
+        }
+
+        c.JSON(http.StatusInternalServerError, gin.H{
+            "error": "Database error",
+            "details": result.Error.Error(),
+        })
+        return
+    }
+
+    c.JSON(http.StatusOK, gin.H{
+        "user": user,
+    })
 }
 
 func CreateUser(c *gin.Context) {

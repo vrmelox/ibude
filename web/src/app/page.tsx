db57fc5c05cd2d9c7e401ac87b5aa164dbfaeb3e
vrmelo <akandeabiodoun@gmail.com>
2026-01-29 14:39:27 +0100

    feat: Add SEO metadata to the home page and introduce a new SVG icon.

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,4 +1,21 @@
-import Image from "next/image";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Ibude – Location de logements uniques entre particuliers",
+  description: "Découvrez Ibude, la plateforme de location entre particuliers pour trouver et réserver des logements uniques, simplement et en toute confiance.",
+  keywords: [
+    "Ibude",
+    "location de logements",
+    "location entre particuliers",
+    "hébergement de vacances",
+    "alternative Airbnb",
+    "réservation logement",
+    "locations courte durée",
+    "séjours authentiques",
+    "plateforme de voyage"
+  ]
+}
+
 
 export default function Home() {
   return (
